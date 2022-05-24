fee7ae47702334983af3108a966e23ad6d5c81e3
João Pedro <83050781+hagsir@users.noreply.github.com>
2022-05-23 22:13:21 -0300

    Update 3- Encanador.c

--- Lista 1/3- Encanador.c
+++ Lista 1/3- Encanador.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 #include<locale.h>
 
-//Uma empresa contrata um encanador a R$ 20,00 por dia. Crie um algoritmo que solicite o
-//número de dias trabalhados pelo encanador e imprima o valor líquido a ser pago, sabendo
-//que são descontados 8% de imposto de renda.
+//Uma empresa deseja contratar um encanador a R$ 20,00 por dia. Crie um algoritmo que solicite o
+//nÃºmero de dias trabalhados pelo encanador e imprima o valor lÃ­quido a ser pago, sabendo
+//que sÃ£o descontados 8% de imposto de renda.
 
 int main(){
 float valor;
 int dias;
 setlocale(LC_ALL, "Portuguese");
 
-    printf("Digite o número de dias trabalhados pelo encanador: ");
+    printf("Digite o nÃºmero de dias trabalhados pelo encanador: ");
     scanf("%d", &dias);
 
     valor = ( dias * 20 ) - (( dias * 20) * 0.08 );
 
-    printf("O valor líquido a ser pago é de %.2f R$ com 8%% de juros descontados.\n" , valor);
+    printf("O valor lÃ­quido a ser pago Ã© de %.2f R$ com 8%% de juros descontados.\n" , valor);
     getch();
 }
