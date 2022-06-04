3d31a5d78c3865c25b89bcb0c18f7549a5026c57
João Pedro <83050781+hagsir@users.noreply.github.com>
2022-06-03 23:39:27 -0300

    Update 10 - Piores notas e alunos.c

--- Lista 7/10 - Piores notas e alunos.c
+++ Lista 7/10 - Piores notas e alunos.c
@@ -9,51 +9,28 @@ caso de empate das piores notas de um aluno, o critério de desempate é
 arbitrário, mas o aluno deve ser contabilizado apenas uma vez.
 */
 int main(){
-int i=0, j, piornota[3], pioraluno[3];
+int i=0, j=0, piornota[3], pioraluno[3];
 float mtz[10][3];
+mtz[i][j]=0;
 setlocale(LC_ALL, "Portuguese");
 
-    printf("Digite a nota dos 10 alunos na primeira prova: ");
-        for(j=0; j<10; j++){
-            scanf("%f", &mtz[i][j]);                            //Leitor das 10 notas na primeira prova
-                if(j==0){
-                    pioraluno[0]=1;
-                    piornota[0]=mtz[i][j];
-                }                                               //Vetor da pior nota e do pior aluno da primeira prova
-                if(piornota[0]>mtz[i][j]){
-                    pioraluno[0]=j+1;
-                    piornota[0]=mtz[i][j];
-                }
-        }
-    i++;
-
-    printf("Digite a nota dos 10 alunos na segunda prova: ");
-        for(j=0; j<10; j++){
-            scanf("%f", &mtz[i][j]);                            //Leitor das 10 notas na segunda prova
-                if(j==0){
-                    pioraluno[1]=1;
-                    piornota[1]=mtz[i][j];
-                }                                               //Vetor da pior nota e do pior aluno da segunda prova
-                if(piornota[1]>mtz[i][j]){
-                    pioraluno[1]=j+1;
-                    piornota[1]=mtz[i][j];
-                }
-        }
-    i++;
-
-    printf("Digite a nota dos 10 alunos na terceira prova: ");
-        for(j=0; j<10; j++){
-            scanf("%f", &mtz[i][j]);                            //Leitor das 10 notas na terceira prova
-                if(j==0){
-                    pioraluno[2]=1;
-                    piornota[2]=mtz[i][j];
-                }                                               //Vetor da pior nota e do pior aluno da terceira prova
-                if(piornota[2]>mtz[i][j]){
-                    pioraluno[2]=j+1;
-                    piornota[2]=mtz[i][j];
-                }
-        }
-    for(i=0; i<3; i++)                                          //Printf do pior aluno, sua pior nota da turma e em qual prova
-    printf("\nO %d° aluno com a nota %d obteve a pior nota na %dª prova.", pioraluno[i], piornota[i], i+1 );
-                                    
+    for(j=0; j<3; j++){
+        system("cls");
+        printf("Digite a nota dos 10 alunos na %d° prova\n:", j+1);
+            for(i=0; i<10; i++){
+                scanf("%f", &mtz[i][j]);                            //Leitor das 10 notas na primeira prova
+                    if(i==0){
+                        pioraluno[j]=i+1;
+                        piornota[j]=mtz[i][j];
+                    }
+                    if(mtz[i][j]<piornota[j]){
+                        pioraluno[j]=i+1;                           //Vetor da pior nota e do pior aluno da primeira prova
+                        piornota[j]=mtz[i][j];
+                    }
+            }
+    }
+    system("cls");
+    for(j=0; j<3; j++)                                          //Printf do pior aluno, sua pior nota da turma e em qual prova
+        printf("O %d° aluno com a nota %d obteve a pior nota na %dª prova.", pioraluno[j], piornota[j], j+1 );
 }
+
